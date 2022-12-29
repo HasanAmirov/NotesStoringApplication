7ae54538ede6ee527225a4539b8728799806013b
HasanAmirov <universityofhasan@gmail.com>
2022-12-30 01:26:28 +0200

    Initial commit

---
+++ app/src/main/java/com/xasan/notesstoringapplication/NoteEditorActivity.java
@@ -0,0 +1,14 @@
+package com.xasan.notesstoringapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+    }
+}
